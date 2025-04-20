bfb4e505f170aff3840aea17d81bdf67f1fc4b5c
ayaanpuri30 <aypuri@ucdavis.edu>
2025-04-19 18:55:23 -0700

    better description and names

--- Frontend/components/hero-section.tsx
+++ Frontend/components/hero-section.tsx
@@ -5,9 +5,9 @@ import { Button } from "@/components/ui/button"
 export default function HeroSection() {
   return (
     <div className="flex flex-col items-center mt-8 text-center md:py-16">
-      <Typewriter text="AID.ai" className="text-9xl font-bold tracking-tighter mb-4" />
+      <Typewriter text="Friend.ly" className="text-9xl font-bold tracking-tighter mb-4" />
       <p className="text-muted-foreground max-w-[600px] mb-8">
-        Come enjoy a conversational experience with our commmunity-based personal AI advisors. Blah blah blah Blah blah blah Blah blah blah Blah blah blah Blah blah blah.
+        Come enjoy a conversational experience with one of our commmunity-based personal AI advisors. Get answers to ANY question from a selection of 100+ professionals. Join the community to customize and publish your own AI advisor.
       </p>
       <div className="flex flex-col sm:flex-row gap-4">
         <Button asChild size="lg">
